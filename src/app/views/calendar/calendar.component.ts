047fa31f4b8ef8c831d8ff01489561054848e0db
AKASMIOU Ouassima <o.akasmiou@etu.enset-media.ac.ma>
2023-08-14 19:03:33 +0100

    Adding calendar component

---
+++ src/app/views/calendar/calendar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent implements OnInit {
+  calendarVisible = signal(true);
+  calendarOptions = signal<CalendarOptions>({
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    events: [
+      
+    ],
+    initialView: 'dayGridMonth',
+    weekends: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
